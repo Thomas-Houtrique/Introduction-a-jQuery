2b0d489c6d8cd34e864cb573d8b4bf851eac9bd5
Thomas Houtrique <30757539+Thomas-Houtrique@users.noreply.github.com>
2020-12-18 15:58:28 +0100

    .

--- js/index.js
+++ js/index.js
@@ -14,20 +14,14 @@ $(".third").click(function(e) {
     $(".card-body").text("happy birthday");
  });
 
- $('.third').click(function (e) { 
-    var obj = document.createElement("audio");
-        obj.src = "son1.mp3"; 
-        obj.play(); 
-});
-
 $('.card').click(function (e) { 
     var obj = document.createElement("audio");
-        obj.src = "son2.mp3"; 
+        obj.src = "son.mp3"; 
         obj.play(); 
 });
 
 $('.btn').click(function (e) { 
     var obj = document.createElement("audio");
-        obj.src = "son2.mp3"; 
+        obj.src = "son.mp3"; 
         obj.play(); 
 });
